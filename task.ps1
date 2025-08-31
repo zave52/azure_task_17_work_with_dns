39b174ff658d572cfc703dd13ef037108e2fc50d
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-31 12:22:36 +0300

    added checking if resource exist before creating for all resources in task.ps1

--- task.ps1
+++ task.ps1
@@ -19,83 +19,194 @@ $dnsLabel = "matetask" + (Get-Random -Count 1)
 
 $privateDnsZoneName = "or.nottodo"
 
-Write-Host "Creating a resource group $resourceGroupName ..."
-New-AzResourceGroup -Name $resourceGroupName -Location $location
+Write-Host "Getting existing resource group ..."
+$resourceGroup = Get-AzResourceGroup -Name $resourceGroupName -Location $location
+
+if ($null -eq $resourceGroup)
+{
+    Write-Host "Resource group does not exist, creating ..."
+    New-AzResourceGroup -Name $resourceGroupName -Location $location
+}
 
 Write-Host "Creating web network security group..."
-$webHttpRule = New-AzNetworkSecurityRuleConfig -Name "web" -Description "Allow HTTP" `
-   -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix `
-   Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80,443
-$webNsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName -Location $location -Name `
-   $webSubnetName -SecurityRules $webHttpRule
+$webNsg = Get-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName -Name $webSubnetName -ErrorAction SilentlyContinue
+if ($null -eq $webNsg)
+{
+    $webHttpRule = New-AzNetworkSecurityRuleConfig -Name "web" -Description "Allow HTTP" `
+       -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix `
+       Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80,443
+    $webNsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName -Location $location -Name `
+       $webSubnetName -SecurityRules $webHttpRule
+    Write-Host "Web network security group created."
+}
+else
+{
+    Write-Host "Web network security group already exists."
+}
 
 Write-Host "Creating mngSubnet network security group..."
-$mngSshRule = New-AzNetworkSecurityRuleConfig -Name "ssh" -Description "Allow SSH" `
-   -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix `
-   Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22
-$mngNsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName -Location $location -Name `
-   $mngSubnetName -SecurityRules $mngSshRule
+$mngNsg = Get-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName -Name $mngSubnetName -ErrorAction SilentlyContinue
+if ($null -eq $mngNsg)
+{
+    $mngSshRule = New-AzNetworkSecurityRuleConfig -Name "ssh" -Description "Allow SSH" `
+       -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix `
+       Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22
+    $mngNsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName -Location $location -Name `
+       $mngSubnetName -SecurityRules $mngSshRule
+    Write-Host "Management network security group created."
+}
+else
+{
+    Write-Host "Management network security group already exists."
+}
 
 Write-Host "Creating a virtual network ..."
-$webSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange -NetworkSecurityGroup $webNsg
-$mngSubnet = New-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -AddressPrefix $mngSubnetIpRange -NetworkSecurityGroup $mngNsg
-$virtualNetwork = New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $webSubnet,$mngSubnet
+$virtualNetwork = Get-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
+if ($null -eq $virtualNetwork)
+{
+    $webSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange -NetworkSecurityGroup $webNsg
+    $mngSubnet = New-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -AddressPrefix $mngSubnetIpRange -NetworkSecurityGroup $mngNsg
+    $virtualNetwork = New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $webSubnet,$mngSubnet
+    Write-Host "Virtual network created."
+}
+else
+{
+    Write-Host "Virtual network already exists."
+}
 
 Write-Host "Creating a SSH key resource ..."
-New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
+$sshKey = Get-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
+if ($null -eq $sshKey)
+{
+    New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
+    Write-Host "SSH key created."
+}
+else
+{
+    Write-Host "SSH key already exists."
+}
 
 Write-Host "Creating a web server VM ..."
-New-AzVm `
--ResourceGroupName $resourceGroupName `
--Name $webVmName `
--Location $location `
--image $vmImage `
--size $vmSize `
--SubnetName $webSubnetName `
--VirtualNetworkName $virtualNetworkName `
--SshKeyName $sshKeyName
-$Params = @{
-    ResourceGroupName = $resourceGroupName
-    VMName = $webVmName
-    Name = 'CustomScript'
-    Publisher = 'Microsoft.Azure.Extensions'
-    ExtensionType = 'CustomScript'
-    TypeHandlerVersion = '2.1'
-    Settings = @{ fileUris = @('https://raw.githubusercontent.com/mate-academy/azure_task_17_work_with_dns/main/install-app.sh'); commandToExecute = './install-app.sh' }
+$webVm = Get-AzVm -ResourceGroupName $resourceGroupName -Name $webVmName -ErrorAction SilentlyContinue
+if ($null -eq $webVm)
+{
+    New-AzVm `
+    -ResourceGroupName $resourceGroupName `
+    -Name $webVmName `
+    -Location $location `
+    -image $vmImage `
+    -size $vmSize `
+    -SubnetName $webSubnetName `
+    -VirtualNetworkName $virtualNetworkName `
+    -SshKeyName $sshKeyName
+    Write-Host "Web server VM created."
 }
-Set-AzVMExtension @Params
+else
+{
+    Write-Host "Web server VM already exists."
+}
+
+# Check if custom script extension already exists
+$extension = Get-AzVMExtension -ResourceGroupName $resourceGroupName -VMName $webVmName -Name 'CustomScript' -ErrorAction SilentlyContinue
+if ($null -eq $extension)
+{
+    $Params = @{
+        ResourceGroupName = $resourceGroupName
+        VMName = $webVmName
+        Name = 'CustomScript'
+        Publisher = 'Microsoft.Azure.Extensions'
+        ExtensionType = 'CustomScript'
+        TypeHandlerVersion = '2.1'
+        Settings = @{ fileUris = @('https://raw.githubusercontent.com/mate-academy/azure_task_17_work_with_dns/main/install-app.sh'); commandToExecute = './install-app.sh' }
+    }
+    Set-AzVMExtension @Params
+    Write-Host "Custom script extension added to web server VM."
+}
+else
+{
+    Write-Host "Custom script extension already exists on web server VM."
+}
 
 Write-Host "Creating a public IP ..."
-$publicIP = New-AzPublicIpAddress -Name $jumpboxVmName -ResourceGroupName $resourceGroupName -Location $location -Sku Standard -AllocationMethod Static -DomainNameLabel $dnsLabel
+$publicIP = Get-AzPublicIpAddress -Name $jumpboxVmName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
+if ($null -eq $publicIP)
+{
+    $publicIP = New-AzPublicIpAddress -Name $jumpboxVmName -ResourceGroupName $resourceGroupName -Location $location -Sku Standard -AllocationMethod Static -DomainNameLabel $dnsLabel
+    Write-Host "Public IP created."
+}
+else
+{
+    Write-Host "Public IP already exists."
+}
+
 Write-Host "Creating a management VM ..."
-New-AzVm `
--ResourceGroupName $resourceGroupName `
--Name $jumpboxVmName `
--Location $location `
--image $vmImage `
--size $vmSize `
--SubnetName $mngSubnetName `
--VirtualNetworkName $virtualNetworkName `
--SshKeyName $sshKeyName `
--PublicIpAddressName $jumpboxVmName
+$jumpboxVm = Get-AzVm -ResourceGroupName $resourceGroupName -Name $jumpboxVmName -ErrorAction SilentlyContinue
+if ($null -eq $jumpboxVm)
+{
+    New-AzVm `
+    -ResourceGroupName $resourceGroupName `
+    -Name $jumpboxVmName `
+    -Location $location `
+    -image $vmImage `
+    -size $vmSize `
+    -SubnetName $mngSubnetName `
+    -VirtualNetworkName $virtualNetworkName `
+    -SshKeyName $sshKeyName `
+    -PublicIpAddressName $jumpboxVmName
+    Write-Host "Management VM created."
+}
+else
+{
+    Write-Host "Management VM already exists."
+}
 
-$Zone = New-AzPrivateDnsZone `
--Name $privateDnsZoneName `
--ResourceGroupName $resourceGroupName
+Write-Host "Creating private DNS zone ..."
+$Zone = Get-AzPrivateDnsZone -Name $privateDnsZoneName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
+if ($null -eq $Zone)
+{
+    $Zone = New-AzPrivateDnsZone `
+    -Name $privateDnsZoneName `
+    -ResourceGroupName $resourceGroupName
+    Write-Host "Private DNS zone created."
+}
+else
+{
+    Write-Host "Private DNS zone already exists."
+}
 
-$Link = New-AzPrivateDnsVirtualNetworkLink `
--ZoneName $privateDnsZoneName `
--ResourceGroupName $resourceGroupName `
--Name "mylink" `
--VirtualNetworkId $virtualNetwork.Id `
--EnableRegistration
+Write-Host "Creating virtual network link ..."
+$Link = Get-AzPrivateDnsVirtualNetworkLink -ZoneName $privateDnsZoneName -ResourceGroupName $resourceGroupName -Name "mylink" -ErrorAction SilentlyContinue
+if ($null -eq $Link)
+{
+    $Link = New-AzPrivateDnsVirtualNetworkLink `
+    -ZoneName $privateDnsZoneName `
+    -ResourceGroupName $resourceGroupName `
+    -Name "mylink" `
+    -VirtualNetworkId $virtualNetwork.Id `
+    -EnableRegistration
+    Write-Host "Virtual network link created."
+}
+else
+{
+    Write-Host "Virtual network link already exists."
+}
 
-$Records = @()
-$Records += New-AzPrivateDnsRecordConfig -Cname $webVmName
-$RecordSet = New-AzPrivateDnsRecordSet `
--Name "todo" `
--RecordType CNAME `
--ResourceGroupName $resourceGroupName `
--TTL 3600 `
--ZoneName $privateDnsZoneName `
--PrivateDnsRecords $Records
+Write-Host "Creating CNAME record ..."
+$RecordSet = Get-AzPrivateDnsRecordSet -ZoneName $privateDnsZoneName -ResourceGroupName $resourceGroupName -Name "todo" -RecordType CNAME -ErrorAction SilentlyContinue
+if ($null -eq $RecordSet)
+{
+    $Records = @()
+    $Records += New-AzPrivateDnsRecordConfig -Cname $webVmName
+    $RecordSet = New-AzPrivateDnsRecordSet `
+    -Name "todo" `
+    -RecordType CNAME `
+    -ResourceGroupName $resourceGroupName `
+    -TTL 3600 `
+    -ZoneName $privateDnsZoneName `
+    -PrivateDnsRecords $Records
+    Write-Host "CNAME record created."
+}
+else
+{
+    Write-Host "CNAME record already exists."
+}
